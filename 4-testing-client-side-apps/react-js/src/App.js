16329f9fdf23e27c41605820a8ed0c9ba99e8469
Frantz Kati <bahdcoder@gmail.com>
2019-12-04 23:28:30 +0100

    Add react and vue projects

--- 4-testing-client-side-apps/react-js/src/App.js
+++ 4-testing-client-side-apps/react-js/src/App.js
@@ -1,24 +1,61 @@
-import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import React, { useState } from 'react'
 
 function App() {
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+    const [errors, setErrors] = useState(null)
+
+    const setValue = (key, value) => setForm({
+        ...form,
+        [key]: value
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        setErrors({
+            email: ['Your email is invalid.'],
+            password: ['Your password is invalid.']
+        })
+    }
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <form onSubmit={handleSubmit} className="form" action="">
+                <h1 className="">Login</h1>
+                <label className="inputLabel" htmlFor="email">
+                    Email
+                </label>
+                <input
+                    className="inputField"
+                    id="email"
+                    placeholder="enter your email"
+                    type="email"
+                    name="email"
+                    value={form.email}
+                    onChange={e => setValue('email', e.target.value)}
+                />
+                {errors && errors.email && <span className='error'>{errors.email}</span>}
+
+                <label className="inputLabel" htmlFor="password">
+                    Password
+                </label>
+                <input
+                    className="inputField"
+                    id="password"
+                    placeholder="enter your password"
+                    type="password"
+                    name="password"
+                    value={form.password}
+                    onChange={e => setValue('password', e.target.value)}
+                />
+                {errors && errors.password && <span className='error'>{errors.password}</span>}
+
+                <button className="loginButton">Login</button>
+            </form>
         </div>
     )
 }
--- 4-testing-client-side-apps/vue/src/App.vue
+++ 4-testing-client-side-apps/vue/src/App.vue
@@ -1,16 +1,69 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
-        <router-view />
+    <div class="App">
+        <form class="form" @submit.prevent='handleSubmit'>
+            <h1 class="">Login</h1>
+            <label class="inputLabel" htmlFor="email">
+                Email
+            </label>
+            <input
+                id="email"
+                type="email"
+                name="email"
+                class="inputField"
+                placeholder="enter your email"
+            />
+            <span class='error' v-if="errors.email">
+                {{ errors.email }}
+            </span>
+
+            <label class="inputLabel" htmlFor="password">
+                Password
+            </label>
+            <input
+                id="password"
+                type="password"
+                name="password"
+                class="inputField"
+                placeholder="enter your password"
+            />
+
+            <span class='error' v-if="errors.password">
+                {{ errors.password }}
+            </span>
+
+            <button class="loginButton">Login</button>
+        </form>
     </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            errors: {}
+        }
+    },
+    methods: {
+        handleSubmit() {
+            this.errors = {
+                email: 'Your email is invalid.',
+                password: 'Your password is invalid.'
+            }
+        }
+    }
+}
+</script>
+
 <style>
+body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+}
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
@@ -29,4 +82,54 @@
 #nav a.router-link-exact-active {
     color: #42b983;
 }
+
+.App {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 40px;
+    margin-top: 80px;
+}
+
+.form {
+    width: 30%;
+}
+
+.inputField {
+    border: none;
+    width: 100%;
+    font-size: 1rem;
+    background: #f2f2f2;
+    padding: 1rem 1.5rem;
+    border-radius: 4px;
+}
+
+.inputLabel {
+    margin: 15px 0;
+    display: inline-block;
+}
+
+.inputField:focus {
+    outline: none;
+}
+
+.loginButton {
+    margin-top: 20px;
+    background: #6abfc3;
+    border-radius: 4px;
+    border: none;
+    padding: 15px 30px;
+    color: #fff;
+    font-size: 1rem;
+    font-weight: bolder;
+    cursor: pointer;
+}
+
+.error {
+    display: inline-block;
+    color: red;
+    width: 100%;
+    margin-top: 10px;
+}
+
 </style>
