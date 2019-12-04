4f328d52c96973c4d45967932ac5aba45cca1878
Frantz Kati <bahdcoder@gmail.com>
2019-12-04 12:06:39 +0100

    prettify all the things

--- 3-end-to-end-testing-apps/controllers/storePost.js
+++ 3-end-to-end-testing-apps/controllers/storePost.js
@@ -3,8 +3,6 @@ const cloudinary = require("cloudinary")
 const Post = require("../database/models/Post")
 
 module.exports = (req, res) => {
-    const { image } = req.files
-
     Post.create(
         {
             ...req.body,
